f46b49538074aa064a6954d06e14e54baf773bfe
SoaresDavidson <davi504r35@gmail.com>
2025-05-02 17:31:27 -0300

    feat:binario da imagem cinza

---
+++ histograma_python/bin_PB.py
@@ -0,0 +1,10 @@
+from PIL import Image
+
+img = Image.open('imagem_pb.jpg').convert('L')
+
+pixel_count = [pixel for pixel in img.getdata()]
+
+pixel_count = bytes(pixel_count)
+
+with open('gray_pixel_count.bin', 'wb') as f:
+    f.write(pixel_count)
