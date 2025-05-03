cf8b1c65fa62e5212921922316d41cf3dc1a6f26
SoaresDavidson <davi504r35@gmail.com>
2025-05-03 18:37:38 -0300

    feat:equalização em ris-v

--- histograma_python/bin_PB.py
+++ histograma_python/bin_PB.py
@@ -3,7 +3,6 @@ from PIL import Image
 img = Image.open('imagem_pb.jpg').convert('L')
 
 pixel_count = [pixel for pixel in img.getdata()]
-print(pixel_count)
 pixel_count = bytes(pixel_count)
 
 with open('gray_pixel_count.bin', 'wb') as f:
